50f661f0eba9a0ea6014cbe443c972be9483a59f
Carlos Eduardo <carlos.oliveira@fiap.com.br>
2023-03-10 14:10:13 -0300

    Criando componente de Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,12 +4,14 @@ import { FlatList } from 'react-native';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
 
 export function Groups() {
 
-  const [groups, setGroups] = useState<string[]>(['Sala 3SIT', 'Sala 3SIS']);
+  const [groups, setGroups] = useState<string[]>(['Turma 3SIT', 'Turma 3SIS']);
 
   return (
     <Container>
@@ -17,7 +19,7 @@ export function Groups() {
 
       <Highlight
         title='Turmas'
-        subtitle='jogue com a sua turma'
+        subtitle='Jogue com a sua turma'
       />
 
       <FlatList
@@ -28,7 +30,12 @@ export function Groups() {
             title={item}
           />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() =>
+          <ListEmpty message='Que tal cadastrar a primeira turma?' />
+        }
       />
+      <Button title='Criar nova turma' />
     </Container>
   )
 }
