c8b51b3990f1d9c384cc7d4def2dea79286495df
Carlos Eduardo <carlos.oliveira@fiap.com.br>
2023-03-20 01:10:53 -0300

    Criando funcao para navegar entre a aplicacao

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { FlatList } from "react-native";
 
+import { useNavigation } from "@react-navigation/native";
+
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -10,7 +12,13 @@ import { Button } from "@components/Button";
 import { Container } from "./styles";
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Turma 3SIT", "Turma 3SIS"]);
+  const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroups() {
+    navigation.navigate("new");
+  }
 
   return (
     <Container>
@@ -25,7 +33,7 @@ export function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroups} />
     </Container>
   );
 }
