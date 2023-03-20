8745460f277353964afdaef227c68972faf81bf2
Carlos Eduardo <carlos.oliveira@fiap.com.br>
2023-03-20 01:20:27 -0300

    Implementando navegacao de voltar goBack navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { BackButton, BackIcon, Container, Logo } from "./styles";
 
+import { useNavigation } from "@react-navigation/native";
+
 import logoImg from "@assets/logo-fiap.png";
 
 type Props = {
@@ -7,10 +9,16 @@ type Props = {
 };
 
 export function Header({ showBackButton = false }: Props) {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.navigate("groups");
+  }
+
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       )}
